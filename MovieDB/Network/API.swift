625a5e744b7de46f72532188a9d990a5bc4800b8
Fábio Salata <fsalata@gmail.com>
2023-05-16 16:03:24 -0300

    Models

--- MovieDB/Network/API.swift
+++ MovieDB/Network/API.swift
@@ -1,10 +1,15 @@
 //
 //  API.swift
-//  Counters
+//  UIKitProjectSeed
+//
+//  Created by Fábio Salata on 05/11/20.
+//  Copyright © 2020 Fábio Salata. All rights reserved.
 //
 
 import Foundation
 
-struct CountersApi: APIProtocol {
-    let baseURL = "http://localhost:3000"
+struct API: APIProtocol {
+    let baseURL = "https://api.themoviedb.org/3/"
+    let backdropURL = "https://image.tmdb.org/t/p/w1280"
+    let posterURL = "https://image.tmdb.org/t/p/w500"
 }
