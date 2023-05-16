efa117bc8d0d1c6eb145db69d2575428bdc6a5aa
Fábio Salata <fsalata@gmail.com>
2023-05-16 15:48:39 -0300

    Creating new SwiftUI Project

---
+++ MovieDB/MovieDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDBApp.swift
+//  MovieDB
+//
+//  Created by Fabio Salata on 16/05/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
